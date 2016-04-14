5c05eb6722ec06b9ac7e5974bf956a6e4c5ce6a5
Fred Qin <fredqin87@gmail.com>
2016-04-13 22:37:20 -0700

    prompt box to enter hours minutes and seconds

--- app/components/taskTimer/taskTimer.ts
+++ app/components/taskTimer/taskTimer.ts
@@ -2,7 +2,7 @@
 
 import { Component, View } from 'angular2/core';
 import { Tasks } from '../../services/tasks';
-import { Button, Icon, TextInput } from 'ionic-framework/ionic';
+import { Button, Icon, TextInput, Alert, NavController} from 'ionic-framework/ionic';
 
 @Component({
     selector: 'task-timer',
@@ -18,7 +18,49 @@ import { Button, Icon, TextInput } from 'ionic-framework/ionic';
 export class TaskTimer {
     private taskService: Tasks;
 
-    constructor(taskService: Tasks) {
+    constructor(taskService: Tasks, public nav: NavController) {
         this.taskService = taskService;
+    }
+
+    setTimer() {
+        let prompt = Alert.create({
+            title: 'Set timer',
+            message: "Enter hours, minutes and seconds",
+            inputs: [
+                {
+                    name: 'hours',
+                    placeholder: 'hours'
+                }, 
+                {
+                    name: 'minutes',
+                    placeholder: 'minutes'
+                },
+                {
+                    name: 'minutes',
+                    placeholder: 'seconds'
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        console.log('Saved clicked');
+                    }
+                }
+            ]
+        });
+
+        // create prompt to input hour, minute and seconds
+        this.nav.present(prompt);
+
+        // input should be limited to 2 digits
+        console.log(prompt)
+
     }
 }
